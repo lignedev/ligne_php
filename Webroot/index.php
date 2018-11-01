8ddf219caee81b6dac2113e062e27045fb09226f
albert <itsalb3rt@gmail.com>
2018-11-01 10:48:48 -0400

    Agregados
    - Se agrego la clase ErrorHandler para el manejo de errores de tipo notices y warning

--- Webroot/index.php
+++ Webroot/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * WebRoot Es el archivo que enlaza todos los mecanismos globales del Framework
-**/
+ **/
 
 //Constantes
 define('WEBROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_NAME"]));
@@ -9,13 +9,13 @@ define('ROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"])
 /**
  * Esta constante define el protocolo utilizado en el momento, es utilizado principlamente para
  * agregar los assets y el redireccionamiento con el metodo del Controller principal
-**/
+ **/
 define('PROTOCOL','http');
 /**
  * Esta constante se define para determinar en que entorno se encuentra la aplicacion usando
  * el framework, esto para evitar mostrar errores no deseados a los usuarios cuando
  * el sistema esta en produccion
-**/
+ **/
 define('ENVIROMENT','dev');
 
 /**
@@ -24,9 +24,10 @@ define('ENVIROMENT','dev');
  * prodria revelar datos que usted no desea que los usuarios sepan
  *
  * Acceso a base de datos, rutas, nombre de clases, directorios, etc...
-**/
+ **/
 function __show_dev_messages__($header,$description,$route = null){
     if(ENVIROMENT == 'dev'){
+        ob_clean (); //Limpia lo que sea que este antes de esta salida
         include (ROOT . 'Core/System/Core/pages_messages/Views/code_messages/messages.php');
         die();
     }else{
@@ -40,18 +41,21 @@ function __show_dev_messages__($header,$description,$route = null){
 /**
  * Carga todas las clases que se utilicen con la
  * palabra clase use, esto es gracias al namespace
-**/
+ **/
 require_once(ROOT . 'Core/System/Core/autoload.php');
 /**
  * El el nucleo centrar el framework, carga clases escenciales
  * para el funcionamiento
-**/
+ **/
 require_once(ROOT . 'Core/System/Core/core.php');
-
 require_once(ROOT . 'router.php');
 require_once(ROOT . 'request.php');
 require_once(ROOT . 'dispatcher.php');
 require_once(ROOT . 'Core/Assets.php');
+//Uses
+use Core\System\Core\errorHandler\ErrorHandler;
+
+new ErrorHandler();
 
 $dispatch = new Dispatcher();
 $dispatch->dispatch();

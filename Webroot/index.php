9f2bef9d2133a4a5fa18231ea75761bf2113a2ed
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2018-12-23 12:22:50 -0400

    Version check

--- Webroot/index.php
+++ Webroot/index.php
@@ -3,6 +3,20 @@
  * WebRoot Es el archivo que enlaza todos los mecanismos globales del Framework
  **/
 
+/**
+ * Verifica la version del servidor, las versiones debajo de la 7.1.3 no soportan
+ * ciertas caracteristicas como la palabra recervada const en propiedades de clases.
+ **/
+if(version_compare(PHP_VERSION, '7.1.3', '>=') == false ){
+    echo '<meta charset="UTF-8">';
+    echo '<title>Versión | Error</title>';
+    echo '<div style="margin: auto;text-align: center;font-family: sans-serif;margin-top: 70px;">';
+    echo '<h1>Versión no compatible</h1>';
+    echo '<h3>La versión de PHP que está utilizando no es compatible con LIGNE, favor utilizar una versión igual o superior a 7.1.3</h3>';
+    echo '</div>';
+    die();
+}
+
 //Constantes
 define('WEBROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_NAME"]));
 define('ROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
